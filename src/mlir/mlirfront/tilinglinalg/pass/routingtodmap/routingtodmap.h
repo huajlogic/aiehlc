dad740c3473cb498e6787c23d06db684ec9d43c1
huajsj <allenhjiang@outlook.com>
2025-11-10 23:42:08 -0800

    add routing to dmap pass

---
+++ src/mlir/mlirfront/tilinglinalg/pass/routingtodmap/routingtodmap.h
@@ -0,0 +1,19 @@
+/******************************************************************************
+* Copyright (C) 2025 Advanced Micro Devices, Inc. All Rights Reserved.
+* SPDX-License-Identifier: MIT
+******************************************************************************/
+
+#ifndef __ROUTING_LOWER__
+#define __ROUTING_LOWER__
+#include "common.h"
+#include "routingmanager.h"
+using namespace mlir;
+class RoutingLowerPass : public PassWrapper<RoutingLowerPass, OperationPass<>> {
+public:
+    RoutingLowerPass(RoutingTopology& rtopology);
+private:
+    void runOnOperation() override;
+    void getDependentDialects(DialectRegistry &registry) const;
+    RoutingTopology& rtopology_;
+};
+#endif //
