f1dbadf8cad81f36b93d2d1e425618f1223f1393
hua jiang <hua.jiang@amd.com>
2026-09-09 15:51:34 -0700

    test(debug): controlpan_pmap edge synthesis

--- src/tool/debug/tests/test_controlpan_pmap.py
+++ src/tool/debug/tests/test_controlpan_pmap.py
@@ -23,3 +23,33 @@ def test_parse_ports_ignores_malformed():
     ports = c.parse_ports(bad)
     assert ports == [{"col": 1, "row": 2, "port": "EAST", "idx": 0,
                       "dir": "fwd", "ms": "master", "id": 7}]
+
+SPINE = """
+CONTROLPAN-PMAP col=0 row=0 port=NORTH idx=0 dir=fwd ms=master id=1
+CONTROLPAN-PMAP col=0 row=1 port=SOUTH idx=0 dir=fwd ms=slave id=1
+CONTROLPAN-PMAP col=0 row=1 port=NORTH idx=0 dir=fwd ms=master id=1
+CONTROLPAN-PMAP col=0 row=2 port=SOUTH idx=0 dir=fwd ms=slave id=1
+"""
+
+def test_edges_spine_pairs_north_master_to_south_slave():
+    edges = c.parse_edges(SPINE)
+    assert {"from": [0, 0], "to": [0, 1], "dir": "fwd", "id": 1, "port": "NORTH"} in edges
+    assert {"from": [0, 1], "to": [0, 2], "dir": "fwd", "id": 1, "port": "NORTH"} in edges
+    assert len(edges) == 2
+
+EAST = """
+CONTROLPAN-PMAP col=0 row=3 port=EAST idx=0 dir=fwd ms=master id=2
+CONTROLPAN-PMAP col=1 row=3 port=WEST idx=0 dir=fwd ms=slave id=2
+"""
+
+def test_edges_east_chain_pairs_east_master_to_west_slave():
+    edges = c.parse_edges(EAST)
+    assert edges == [{"from": [0, 3], "to": [1, 3], "dir": "fwd", "id": 2, "port": "EAST"}]
+
+def test_edges_ctrl_is_not_an_edge():
+    ctrl = "CONTROLPAN-PMAP col=0 row=3 port=CTRL idx=0 dir=fwd ms=master id=1\n"
+    assert c.parse_edges(ctrl) == []
+
+def test_parse_summary_counts():
+    r = c.parse(SPINE)
+    assert r["count"] == 4 and len(r["edges"]) == 2
